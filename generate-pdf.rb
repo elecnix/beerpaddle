331258280c76cad3a0237c6d630214fe52826e4f
Nicolas Marchildon <nicolas@marchildon.net>
2018-09-23 18:28:44 -0400

    Generate PDF, 6 beers per page

--- generate-pdf.rb
+++ generate-pdf.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require './models/beer'
 require './models/brewery'
+require "prawn"
 
 rows = CSV.table("beers.csv")
 
@@ -11,9 +12,53 @@ breweries = rows.reduce({}) do |b, row|
 end
 
 beers = rows.map do |row|
-  Beer.new(row[:name], breweries[row[:brewery]])
+  Beer.new(row[:name], breweries[row[:brewery]], row[:type], row[:abv], row[:ibu], row[:description])
 end
 
 beers.each do |beer|
   print "#{beer.name}\t#{beer.brewery.name}\n"
+end
+
+Prawn::Document.generate("out.pdf") do
+  beers.each_slice(6).each_with_index do |page, index|
+    if (index != 0) then
+      start_new_page
+    end
+    page.each do |beer|
+      top_margin = bottom_margin = 5
+      left_margin = right_margin = 5
+      columns = 3
+      bounding_box([0, cursor - top_margin], :width => bounds.width, :height => 110) do
+        #stroke_bounds
+        bounding_box([left_margin, cursor - top_margin], :width => bounds.width - left_margin - right_margin, :height => bounds.height) do
+          float do
+            bounding_box([bounds.width / columns, cursor - top_margin], :width => bounds.width / columns * 2) do
+              text "#{beer.description}"
+            end
+          end
+          font("Helvetica", :style => :bold, :size => 15) do
+            text_box beer.name, :width => bounds.width / columns, :height => 20, :overflow => :shrink_to_fit
+          end
+          move_down 18
+          bounding_box([0, cursor], :width => bounds.width / columns) do
+            font("Helvetica", :style => :bold, :size => 12) do
+              text_box beer.brewery.name, :width => bounds.width, :height => 20, :overflow => :shrink_to_fit
+            end
+          end
+          move_down 14
+          bounding_box([0, cursor], :width => bounds.width / columns) do
+            formatted_text [{ :text => "#{beer.type}", :styles => []}]
+          end
+          bounding_box([0, cursor], :width => bounds.width / columns) do
+            text "#{beer.abv || 'N/A'}%  #{beer.ibu || 'N/A'} IBU"
+          end
+          bounding_box([0, cursor], :width => bounds.width, :height => cursor - left_margin - bottom_margin) do
+            text_box "Notes", :at => [left_margin, bounds.height - top_margin], :size => 10
+            stroke_color "aaaaaa"
+            stroke_bounds
+          end
+        end
+      end
+    end
+  end
 end
--- models/beer.rb
+++ models/beer.rb
@@ -1,8 +1,12 @@
 class Beer
 
-    def initialize(name, brewery)
+    def initialize(name, brewery, type=nil, abv=nil, ibu=nil, description=nil)
         @name = name
         @brewery = brewery
+        @type = type
+        @abv = abv
+        @ibu = ibu
+        @description = description
     end
 
     def name
@@ -11,6 +15,22 @@ class Beer
 
     def brewery
         @brewery
+    end
+
+    def type
+        @type
+    end
+
+    def abv
+        @abv
+    end
+
+    def ibu
+        @ibu
+    end
+
+    def description
+        @description
     end
 
 end
